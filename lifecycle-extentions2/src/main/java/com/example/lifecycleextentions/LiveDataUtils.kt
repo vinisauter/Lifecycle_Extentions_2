71214a575ceaabbd9fb9d0e818d5763063bf2cf2
vsauter <vsauter@brq.com>
2019-10-24 09:47:43 -0300

    Initial Commit

--- lifecycle-extentions2/src/main/java/com/example/lifecycleextentions/LiveDataUtils.kt
+++ lifecycle-extentions2/src/main/java/com/example/lifecycleextentions/LiveDataUtils.kt
@@ -29,8 +29,8 @@ fun <T> LiveData<T>.defaultIfNull(default: T): LiveData<T> {
 fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
     observe(lifecycleOwner, object : Observer<T> {
         override fun onChanged(t: T?) {
-            observer.onChanged(t)
             removeObserver(this)
+            observer.onChanged(t)
         }
     })
 }
